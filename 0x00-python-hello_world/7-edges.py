b1a9d3aedfd28ea794eab2c9b9a23f955777aa96
DAUDA SHERIFFDEEN <s.dayo36@yahoo.com>
2022-12-06 07:21:40 +0000

    task 7 updated

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[7:]
-middle_word = word[1:8]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print(f"{word_first_3}")
 print(f"{word_last_2}")
 print(f"{middle_word}")
