c696414412942f08bbfe17e21b954f8720564928
DAUDA SHERIFFDEEN <s.dayo36@yahoo.com>
2022-12-06 07:10:57 +0000

    task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print(f"{word_first_3}")
+print(f"{word_last_2}")
+print(f"{middle_word}")
