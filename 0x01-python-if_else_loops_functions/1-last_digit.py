dd389fb36cd8bff6af6693eb3f95aa5d88d52e08
DAUDA SHERIFFDEEN <s.dayo36@yahoo.com>
2022-12-24 08:45:11 +0000

    1 lastdgt

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-lastdgt = abs(number) % 10
-
+if number >= 0:
+    lastdgt = abs(number) % 10
+else:
+    lastdgt = -((abs(number)) % 10)
 if lastdgt > 5:
     print(f"Last digit of {number} is {lastdgt} and is greater than 5")
 if lastdgt == 0:
