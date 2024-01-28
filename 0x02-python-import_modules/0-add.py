042bd6090ace6f777d2f0db99aa0caf8c591d8dd
root <s.dayo36@yahoo.com>
2024-01-28 09:21:08 -0800

    task 0 & 1 editted

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     from add_0 import add
 
-    y = 1
-    x = 2
-    print('{:d} + {:d} = {:d}'.format(y, x, add(y, x)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    y = 10
-    x = 5
-    print("{:d} + {:d} = {:d}".format(y, x, add(y, x)))
-    print("{:d} - {:d} = {:d}".format(y, x, sub(y, x)))
-    print("{:d} * {:d} = {:d}".format(y, x, mul(y, x)))
-    print("{:d} / {:d} = {:d}".format(y, x, div(y, x)))
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
